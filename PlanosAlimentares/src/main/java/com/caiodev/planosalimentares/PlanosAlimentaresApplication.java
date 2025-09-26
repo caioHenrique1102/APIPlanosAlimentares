d7a8bc4e73279f4d45c0e234184954c0535cbdba
Caio <ntjrio7@gmail.com>
2025-09-25 21:04:53 -0300

    Project created

---
+++ PlanosAlimentares/src/main/java/com/caiodev/planosalimentares/PlanosAlimentaresApplication.java
@@ -0,0 +1,13 @@
+package com.caiodev.planosalimentares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanosAlimentaresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanosAlimentaresApplication.class, args);
+    }
+
+}
---
+++ PlanosAlimentares/src/test/java/com/caiodev/planosalimentares/PlanosAlimentaresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiodev.planosalimentares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanosAlimentaresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
